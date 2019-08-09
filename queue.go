b0af9a20aec4bb9d9d45fe89a1a2b5f8f7f01bad
Nick Hughes <nick.hughes@centene.com>
2019-08-09 12:44:05 -0500

    clean up queue implementation

--- queue.go
+++ queue.go
@@ -4,7 +4,7 @@ import (
 	"container/heap"
 )
 
-// An Node is something we manage in a priority queue.
+// Node holds a stringified Board state and it's corresponding priority
 type Node struct {
 	value    string // The value of the node; arbitrary.
 	priority int    // The priority of the node in the queue.
@@ -12,24 +12,25 @@ type Node struct {
 	index int // The index of the node in the heap.
 }
 
-// A PriorityQueue implements heap.Interface and holds Items.
+// PriorityQueue implements heap.Interface and holds Nodes.
 type PriorityQueue []*Node
 
 func (pq PriorityQueue) Len() int { return len(pq) }
 
 func (pq PriorityQueue) Less(i, j int) bool {
-	// We want Pop to give us the lowest, not highest, cost priority so we use less than here.
+	// We want Pop to give us the lowest, not highest,
+	// cost priority so we use less than here.
 	return pq[i].priority < pq[j].priority
 }
 
-//Swap ...
+// Swap ...
 func (pq PriorityQueue) Swap(i, j int) {
 	pq[i], pq[j] = pq[j], pq[i]
 	pq[i].index = i
 	pq[j].index = j
 }
 
-//Push ...
+// Push ...
 func (pq *PriorityQueue) Push(x interface{}) {
 	n := len(*pq)
 	node := x.(*Node)
@@ -37,7 +38,7 @@ func (pq *PriorityQueue) Push(x interface{}) {
 	*pq = append(*pq, node)
 }
 
-//Pop ...
+// Pop ...
 func (pq *PriorityQueue) Pop() interface{} {
 	old := *pq
 	n := len(old)
@@ -47,46 +48,9 @@ func (pq *PriorityQueue) Pop() interface{} {
 	return node
 }
 
-//Update modifies the priority and value of an Node in the queue.
+// Update modifies the priority and value of an Node in the queue.
 func (pq *PriorityQueue) Update(node *Node, value string, priority int) {
 	node.value = value
 	node.priority = priority
 	heap.Fix(pq, node.index)
 }
-
-// This example creates a PriorityQueue with some items, adds and manipulates an node,
-// and then removes the items in priority order.
-// func main() {
-// 	// Some items and their priorities.
-// 	items := map[string]int{
-// 		"banana": 3, "apple": 2, "pear": 4,
-// 	}
-
-// 	// Create a priority queue, put the items in it, and
-// 	// establish the priority queue (heap) invariants.
-// 	pq := make(PriorityQueue, len(items))
-// 	i := 0
-// 	for value, priority := range items {
-// 		pq[i] = &Node{
-// 			value:    value,
-// 			priority: priority,
-// 			index:    i,
-// 		}
-// 		i++
-// 	}
-// 	heap.Init(&pq)
-
-// 	// Insert a new node and then modify its priority.
-// 	node := &Node{
-// 		value:    "orange",
-// 		priority: 1,
-// 	}
-// 	heap.Push(&pq, node)
-// 	pq.Update(node, node.value, 5)
-
-// 	// Take the items out; they arrive in decreasing priority order.
-// 	for pq.Len() > 0 {
-// 		node := heap.Pop(&pq).(*Node)
-// 		fmt.Printf("%.2d:%s ", node.priority, node.value)
-// 	}
-// }
